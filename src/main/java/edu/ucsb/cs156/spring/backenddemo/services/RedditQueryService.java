3b491c7b30a66c64391a3c1c263a4d5e27f2dcec
Joseph Collins <josephcollins@ucsb.edu>
2022-10-19 18:49:22 -0700

    trying to fix something

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/services/RedditQueryService.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/services/RedditQueryService.java
@@ -2,7 +2,9 @@ package edu.ucsb.cs156.spring.backenddemo.services;
 
 import java.util.List;
 import java.util.Map;
+import java.io.StringReader;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.client.RestTemplate;
 
 import lombok.extern.slf4j.Slf4j;
@@ -19,6 +21,8 @@ import org.springframework.web.client.HttpClientErrorException;
 @Slf4j
 @Service
 public class RedditQueryService {
+
+    private ObjectMapper mapper = new ObjectMapper();
 
     private final RestTemplate restTemplate;
 
@@ -37,9 +41,9 @@ public class RedditQueryService {
 
         // Add to this section
 
-        HttpEntity<String> entity = new HttpEntity<>(headers);
-
         Map<String, String> uriVariables = Map.of("subreddit", subreddit);
+
+        HttpEntity<String> entity = new HttpEntity<>("body", headers);
 
         ResponseEntity<String> re = restTemplate.exchange(ENDPOINT, HttpMethod.GET, entity, String.class,
                 uriVariables);
